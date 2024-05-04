52869e7dc80577f5494b87f2419b268b4d8259ff
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-05-04 12:17:36 -0400

    working on figuring out how to count multibyte characters

--- ccwc.cc
+++ ccwc.cc
@@ -12,10 +12,11 @@
 
 using namespace std; 
 
-int readFile(string filename, int line, int words) {
+int readFile(string filename, int line, int words, int chars) {
    ifstream input; 
    input.open(filename); 
    string word; 
+   char letter; 
    if (!input) {
       // file does not exist 
       return -1; 
@@ -30,12 +31,34 @@ int readFile(string filename, int line, int words) {
       return num_lines; 
 
    }
+   // counting words 
    if (words) {
       int num_words = 0; 
       while (input >> word) {
          num_words++; 
       }
       return num_words; 
+   }
+   if (chars) {
+      int num_chars = 0; 
+      int extra_chars = 0; 
+      while (input >> word) {
+         num_chars+= word.size(); 
+         num_chars++; 
+         // if (mblen(&letter,0) == 1) {
+         //    cout << "extra" << endl; 
+         //    extra_chars++; 
+         // }
+         // else {
+         //    num_chars++; 
+         // }
+      }
+      //num_chars += (extra_chars / 4); 
+      // while (getline(input, word)) {
+      //    num_chars += word._mblen(); 
+      // }
+   
+      return num_chars; 
    }
    // generic case 
    input.seekg(0, ios::end); 
@@ -58,6 +81,7 @@ int main(int argc, char** argv) {
    int arg = 1;
    int l = 0;  
    int w = 0; 
+   int c = 0; 
    // iterate through the flags to get to the file 
    while (arg < argc && argv[arg][0] == '-') {
       // if (argv[arg].length() != 2) {
@@ -70,6 +94,9 @@ int main(int argc, char** argv) {
       if (argv[arg][1] == 'w') {
          w = 1; 
       }
+      if (argv[arg][1] == 'm') {
+         c = 1; 
+      }
       arg++; 
    }
    if (arg == argc) {
@@ -79,7 +106,7 @@ int main(int argc, char** argv) {
    // open the file 
    //input.open(argv[arg]); 
    filename = argv[arg]; 
-   long chars = readFile(filename, l, w); 
+   long chars = readFile(filename, l, w, c); 
    cout << chars << " " << filename << endl; 
    return 0;
 
